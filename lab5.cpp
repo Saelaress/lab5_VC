e8918c67f94723a9721adc8ff320afe24ce78345
Nadezhda Kosyura <nadejdakna2002@gmail.com>
2023-04-16 23:29:07 +0300

    select sections and places for commits

--- lab5.cpp
+++ lab5.cpp
@@ -6,6 +6,7 @@
 
 int main()
 {
+	//////////// main area ////////////
 	int i, M, sum;
 	sum = 0;
 	char string, text[20][81];
@@ -13,36 +14,45 @@ int main()
 	printf("Введите количество строк(максимально 21): ");
 	scanf_s("%d", &M);
 	getchar();
-	
+	// main 1
 	if ((M > 21) || (M < 1)) {
+		// main 2
 		error_printf("Неверное количество строк");
+		// main 3
 		return 0;
 	}
-
+	
 	for (i = 0; i < M; i++) {
+			// main 4
 		puts("Введите содержимое строки: ");
  		gets_s(text[i]);
 		strlen(text[i]);
 		if ((strlen(text[i]) > 81) || (strlen(text[i]) < 1)) {
+			// main 5
 			error_printf("Неправильное содержимое строки");
 			return 0;
 		}
 	}
 	printf("\n");
 	
+	//////////// second area ////////////
 	for (i = 0; i < M; i++) {
+		// second 1
 		sum += strlen(text[i]);
 	}
-
+	// second 2
 	for (i = 0; i < M; i++) {
+		// second 3
 		if (isalnum(text[i + 1][0])) {
 			_strrev(text[i]);
+			//second 4
 			if (isalnum(text[i][0])) {
 				_strrev(text[i]);
 				strcat_s(DOC, text[i]); strcat_s(DOC, " ");
 				strcat_s(DOC, text[i + 1]);
 				i++;
 			}
+			// second 5
 			else {
 				_strrev(text[i]);
 			}
@@ -51,21 +61,27 @@ int main()
 			strcat_s(DOC, text[i + 1]);
 	}
 	
+	//////////// third area ////////////
 	DOC[sum] = '\0';
 	char COD[1621] = "";
 	strcpy(COD, DOC);
 	COD[sum] = '\0';
 	char* istr;
 
+	// third 1
 	char seps[] = "?!.";
 	char* next_token;
 	char* token;
 	
+		// third 2
 		token = strtok_s(DOC, seps, &next_token);
 		
-		while (token != NULL) {		
+		// third 3
+		while (token != NULL) {	
+			// third 4
 			puts(token);
 			token = strtok_s(NULL, seps, &next_token);
+			// third 5
 			istr = strstr(COD, token);
 			printf("%s", COD[istr-token+1]);
 
